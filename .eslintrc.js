877f6c37a8b9610e47f63030e4eebd890b3ec840
Bruno Sampaio <bens.sampaio@gmail.com>
2018-03-06 11:48:34 +0100

    Support webpack 4 (#44)
    * Upgrade to Webpack 4
    * Upgrade all dependencies
    * Use new Tapable API
    * Use iterables instead of `forEach` and `map` functions on Chunks and Modules
    * Rewrite React components as stateless components
    * Add missing documentation
    * Improve plugin documentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     "globals": {
         "document": true,
     },
-    "installedESLint": true,
     "parserOptions": {
         "ecmaFeatures": {},
         "sourceType": "module"
